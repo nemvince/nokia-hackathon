2fcb71a7340ab77b7aa99ae57e10527af47f18cc
Tamás Vince Kornél <vin@kys.social>
2024-05-24 01:19:51 +0200

    palindrome checker

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,15 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+import re
 
-print(input)
+with open('./input.txt', 'r') as f:
+  data = f.read().strip().split("\n")
+
+pattern = re.compile(r'[\W_]+')
+
+for word in data:
+  word = pattern.sub('', word)
+  word = word.lower()
+  if word == word[::-1]:
+    unique = len(set(word))
+    print(f"YES, {unique}")
+  else:
+    print("NO, -1")
